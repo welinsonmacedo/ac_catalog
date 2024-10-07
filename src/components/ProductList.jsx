e0e3564165b11b0475b1f4d9a8bd3b47c9520eca
welinsonmacedo <welinsonmarlon15@gmail.com>
2024-10-06 23:13:03 -0300

    Add Products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -116,6 +116,253 @@ const products = [
       'Vestidos/Stefany2.jpg',
     ]
   },
+  {
+    id: 1,
+    name: 'Vestido Lídia',
+    price: '3.499,99',
+    priceDesc:' 2.099,99',
+    size: '38',
+    color: 'verde abacate ',
+    images: [
+      'Vestidos/lidia.png',
+      'Vestidos/lidia2.png',
+      'Vestidos/lidia3.png',
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Anne',
+    price: '3.499,98',
+    priceDesc:' 2.099,99',
+    size: '46',
+    color: 'lilás',
+    images: [
+      'Vestidos/AnneC.png',
+      'Vestidos/AnneC2.png',
+      
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Anne',
+    price: '3.499,98',
+    priceDesc:' 2.099,99',
+    size: '42 ,44',
+    color: 'verde água ',
+    images: [
+      'Vestidos/AnneV.png',
+      'Vestidos/AnneV2.png',
+      'Vestidos/AnneV3.png',
+      
+      
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Ofélia',
+    price: '3.499,98',
+    priceDesc:' 2.099,99',
+    size: '42 ,44',
+    color: 'verde folha ',
+    images: [
+      'Vestidos/orfelia.png',
+      'Vestidos/orfelia2.png',
+    
+      
+      
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Julia ',
+    price: '9.499,99 ',
+    priceDesc:' 5.969,99',
+    size: '38 ',
+    color: 'Vinho ',
+    images: [
+      'Vestidos/julia3.png',
+      'Vestidos/julia2.png',
+      'Vestidos/julia.png',
+     
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Anastácia',
+    price: '529,99  ',
+    priceDesc:' 254,99',
+    size: '38 ',
+    color: 'Azul',
+    images: [
+      'Vestidos/anastacia.png',
+      'Vestidos/anastacia2.png',
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Vanessa ',
+    price: '499,99 ',
+    priceDesc:' 299,99',
+    size: '38 ',
+    color: 'verde militar ',
+    images: [
+      'Vestidos/vanessap.png',
+      'Vestidos/vanessap2.png',
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Vestido Ariel',
+    price: '3499,99',
+    priceDesc:' 2099,99 ',
+    size: '42',
+    color: 'lilás',
+    images: [
+      'Vestidos/ariel.png',
+    
+    ]
+  },
+  {
+    id: 1,
+    name: 'Chemise Amara',
+    price: '499,99',
+    priceDesc:' 299,99  ',
+    size: '38',
+    color: 'branca ',
+    images: [
+      'ChemiseAmara/chemisebranca.png',
+      'ChemiseAmara/chemisebranca2.png',
+      
+    ]
+  },
+  {
+    id: 1,
+    name: 'Chemise Amara',
+    price: '499,99',
+    priceDesc:' 299,99  ',
+    size: '40',
+    color: 'Bege',
+    images: [
+      'ChemiseAmara/chemisebege.png',
+      'ChemiseAmara/chemisebege2.png',
+    
+    ]
+  },
+  {
+    id: 1,
+    name: 'Chemise Amara',
+    price: '499,99',
+    priceDesc:' 299,99  ',
+    size: '38',
+    color: 'amarela',
+    images: [
+      'ChemiseAmara/chemiseamarela.png',
+      'ChemiseAmara/chemiseamarela2.png',
+      'ChemiseAmara/chemiseamarela3.png',
+    ]
+  },
+  {
+    id: 1,
+    name: 'Conjuto Alice',
+    price: '824,99',
+    priceDesc:' 499,99 ',
+    size: '38',
+    color: 'linho cru',
+    images: [
+      'Conjunto/alice.png',
+      'Conjunto/alice2.png',
+      'Conjunto/alice3.png',
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Conjuto Alice',
+    price: '824,99',
+    priceDesc:' 499,99 ',
+    size: '38,40',
+    color: 'linho cru',
+    images: [
+      'Conjunto/alicem.png',
+      'Conjunto/alicem2.png',
+      'Conjunto/alicem3.png',
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Conjuto Alice',
+    price: '824,99',
+    priceDesc:' 499,99 ',
+    size: '44',
+    color: 'Rose',
+    images: [
+      'Conjunto/alicerose.png',
+      'Conjunto/alicerose2.png',
+     
+    ]
+  },
+  {
+    id: 1,
+    name: 'Conjuto Alice',
+    price: '824,99',
+    priceDesc:' 499,99 ',
+    size: '38',
+    color: 'Laranja',
+    images: [
+      'Conjunto/alicelaranja.png',
+      'Conjunto/alicelaranja2.png',
+      'Conjunto/alicelaranja3.png',
+   
+    ]
+  },
+  {
+    id: 1,
+    name: 'Blusa Camila',
+    price: '264,99',
+    priceDesc:' 158,99 ',
+    size: '38,44',
+    color: 'Branca',
+    images: [
+      'Blusas/camila.png',
+      
+   
+    ]
+  },
+  {
+    id: 1,
+    name: 'Blusa Mayra',
+    price: '524,99',
+    priceDesc:' 314,99',
+    size: '40',
+    color: 'Cinza',
+    images: [
+      'Blusas/mayra.png',
+      
+   
+    ]
+  },
+  {
+    id: 1,
+    name: 'Camisa Rafa',
+    price: '629,99',
+    priceDesc:' 374,99',
+    size: '38',
+    color: 'Verde',
+    images: [
+      'Blusas/rafa.png',
+      
+   
+    ]
+  },
+ 
  
 ];
 
