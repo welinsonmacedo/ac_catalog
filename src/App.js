565655f9e03a58337723b71ba758287a4f2058fb
welinsonmacedo <welinsonmarlon15@gmail.com>
2024-10-07 21:18:37 -0300

    Edit Design Padding all

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import ProductList from './components/ProductList';
 import Title from './components/Tittle';
 import'./global.css'
+const Container = styled.div`
+padding:5px;
+
+  @media (max-width: 768px) {
+ 
+    padding: 20px;
+  }
+`;
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Title/>
       <ProductList />
-    </div>
+    </Container>
   );
 }
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,6 +6,7 @@ display: flex;
 flex-direction: column;
 align-items: center;
 justify-content:center;
+padding: 15px;
 
 `;
 const products = [
